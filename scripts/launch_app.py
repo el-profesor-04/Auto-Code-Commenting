47729164b606a0364626bfd5fc770b34d2d100e9
Narendra G <56555663+el-profesor-04@users.noreply.github.com>
2023-02-01 14:41:42 -0600

    Update launch_app.py

--- scripts/launch_app.py
+++ scripts/launch_app.py
@@ -1,4 +1,4 @@
 !export PATH="$/home/cdsw/.local/lib/python3.7/site-packages:$PATH"
 
-!flask --app app/app.py run --port=5000 --host=127.0.0.1
+!flask --app app/app.py run --port=$CDSW_APP_PORT --host=127.0.0.1
  
