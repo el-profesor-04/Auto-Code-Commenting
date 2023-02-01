61ea0973447b4bd739c0b58c0fde596847c68ac3
narendra <myemail@gmail.com>
2023-01-31 19:56:33 -0600

    update

--- scripts/launch_app.py
+++ scripts/launch_app.py
@@ -1,5 +1,5 @@
 !export PATH="$/home/cdsw/.local/lib/python3.7/site-packages:$PATH"
 
 !which python
-!flask --app app/app.py run --port $5000 --host 127.0.0.1
+!flask --app app/app.py run --port=5000 --host=127.0.0.1
  
